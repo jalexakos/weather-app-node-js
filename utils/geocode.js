54a6ebbf16690cc6ad848fcf3e17e265a28339f3
jalexakos <64025205+jalexakos@users.noreply.github.com>
2021-01-14 20:53:26 -0500

    Update geocode.js

--- utils/geocode.js
+++ utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiamFsZXhha29zIiwiYSI6ImNrang2eWNzMzAxN28yb3F3dnFseTRtc2IifQ.2_QQVc4GHcUEu3ldGH7cdw&limit=1&language=en'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=[ACCESS KEY HIDDEN]&limit=1&language=en'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
