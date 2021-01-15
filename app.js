90eab69e8fb9677f47b3bba1a4609bacd3739dcb
jalexakos <josiah.alexakos@gmail.com>
2021-01-14 20:51:34 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const location = process.argv[2]
+
+if (!location) {
+    console.log('Please provide an address!')
+} else {
+    geocode(location, (error, {latitude, longitude, location} ={}) => {
+        if (error) {
+            return console.log(error)
+        }
+    
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+    
+            console.log(location)
+            console.log(forecastData)
+          })
+    })
+}
